cd7cab4cb597bc091e24219915894e1911b7859a
Brandon Fernandez <fernandez.b@dhn.com.do>
2022-09-27 13:04:09 -0400

    added router

--- src/screens/OrderDelivery/index.js
+++ src/screens/OrderDelivery/index.js
@@ -1,31 +1,170 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react'
-import { View, Text, StyleSheet, useWindowDimensions } from 'react-native'
+import { View, Text, StyleSheet, useWindowDimensions, ActivityIndicator, Pressable } from 'react-native'
 import BottomSheet from '@gorhom/bottom-sheet';
+import { Entypo, MaterialIcons, FontAwesome5, Fontisto } from '@expo/vector-icons'
+import MapViewDirections from 'react-native-maps-directions';
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from "expo-location"
-import { FontAwesome5, Fontisto } from '@expo/vector-icons'
+import styles from './styles';
 
 import orders from '../../../assets/data/orders.json'
-import styles from './styles';
-import MapView, { Marker } from 'react-native-maps';
 
 const order = orders[0]
 
-const OrderDeliver = () => {
 
+const ORDER_STATYES = {
+   READY_FOR_PICKUP: "READY_FOR_PICKUP",
+   ACCEPTED: "ACCEPTED",
+   PICKED_UP: "PICKED_UP"
+}
+
+const OrderDeliver = () => {
+   const [driverLocation, setDriverLocation] = useState(null)
+   const [totalMin, setTotalMin] = useState(0)
+   const [totalKm, setTotalKm] = useState(0)
+   const [deliveryStatus, setDeliveryStatus] = useState(ORDER_STATYES.READY_FOR_PICKUP)
+   const [isDriverClose, setIsDriverClose] = useState(false)
+   const mapRef = useRef(null)
    const { width, height } = useWindowDimensions()
    const bottomSheetRef = useRef(null);
-   const snapPoints = useMemo(() => ["20%", "95%"], [])
+
+   const snapPoints = useMemo(() => ["25%", "95%"], [])
 
 
+
+   useEffect(() => {
+      (async () => {
+
+         let { status } = await Location.requestForegroundPermissionsAsync()
+         if (!status == "granted") {
+            console.log('NONONO');
+            return;
+         }
+
+         let location = await Location.getCurrentPositionAsync()
+
+         setDriverLocation({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude
+         });
+      })()
+
+      const foregroundSubscription = Location.watchPositionAsync(
+         {
+            accuracy: Location.Accuracy.High,
+            distanceInterval: 100
+         }, (updatedLocation) => {
+            setDriverLocation({
+               latitude: updatedLocation.coords.latitude,
+               longitude: updatedLocation.coords.longitude
+            })
+         }
+      )
+
+      return foregroundSubscription;
+
+   }, [])
+
+
+   if (!driverLocation) {
+      return <ActivityIndicator size={"large"} />
+   }
+
+   const onButtonPressed = () => {
+      if(deliveryStatus === ORDER_STATYES.READY_FOR_PICKUP){
+         bottomSheetRef.current?.collapse();
+         mapRef.current.animateToRegion({
+            latitude: driverLocation.latitude,
+            longitude: driverLocation.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
+         })
+
+         setDeliveryStatus(ORDER_STATYES.ACCEPTED);
+      }
+   }
+
+   const renderButtonTitle = () => {
+      if(deliveryStatus === ORDER_STATYES.READY_FOR_PICKUP){
+         return "Accept Order"
+      }
+
+      if(deliveryStatus === ORDER_STATYES.ACCEPTED){
+         return "Pick-Up Order"
+      }
+
+      if(deliveryStatus === ORDER_STATYES.PICKED_UP){
+         return "Complete Delivery"
+      }
+   }
+
+   const isButtonDisabled = () => {
+      if(deliveryStatus === ORDER_STATYES.READY_FOR_PICKUP) {
+         return false
+      }
+
+      if(deliveryStatus === ORDER_STATYES.ACCEPTED && isDriverClose){
+         return false
+      }
+
+      if(deliveryStatus ===  ORDER_STATYES.PICKED_UP && isDriverClose){
+         return false
+      }
+
+      return true
+   }
 
    return (
       <View style={styles.container}>
          <MapView
+            ref={mapRef}
             style={{ width: width, height: height }}
             showsUserLocation
             followsUserLocation
+            initialRegion={{
+               latitude: driverLocation?.latitude,
+               longitude: driverLocation?.longitude,
+               latitudeDelta: 0.07,
+               longitudeDelta: 0.07
+            }}
          >
+            <MapViewDirections
+               origin={driverLocation}
+               destination={deliveryStatus === ORDER_STATYES.ACCEPTED ? { latitude: order.Restaurant.lat, longitude: order.Restaurant.lng } : { latitude: order.User.lat, longitude: order.User.lng } }
+               strokeWidth={10}
+               waypoints={deliveryStatus === ORDER_STATYES.READY_FOR_PICKUP ? 
+                  [{ latitude: order.Restaurant.lat, longitude: order.Restaurant.lng }] : []
+               }
+               strokeColor="#3FC060"
+               apikey={"AIzaSyBL_jCIzt9pEeYmcpABj0AeEq4zZpdcyVc"}
+               onReady={(result) => {
+                  if(result.distance <= 0.1){
+                     return setIsDriverClose(true)
+                  }
+                  setTotalMin(result.duration);
+                  setTotalKm(result.distance);
+               }}
 
+            />
+            <Marker
+               coordinate={{ latitude: order.Restaurant.lat, longitude: order.Restaurant.lng }}
+               title={order.Restaurant.name}
+               description={order.Restaurant.address}
+            >
+               <View style={{ backgroundColor: 'green', borderRadius: 25, padding: 5 }}>
+                  <Entypo name='shop' size={30} color="white" />
+               </View>
+            </Marker>
+
+            <Marker
+               coordinate={{ latitude: order.User.lat, longitude: order.User.lng }}
+               title={order.User.name}
+               description={order.User.address}
+            >
+               <View style={{ backgroundColor: 'green', borderRadius: 25, padding: 5 }}>
+                  <MaterialIcons name='restaurant' size={30} color="white" />
+               </View>
+            </Marker>
          </MapView>
          <BottomSheet
             ref={bottomSheetRef}
@@ -34,14 +173,14 @@ const OrderDeliver = () => {
          >
             <View
                style={styles.handleIndicatorContainer}>
-               <Text style={styles.routeDetailsText}>15 min</Text>
+               <Text style={styles.routeDetailsText}>{totalMin.toFixed(0)} min</Text>
                <FontAwesome5
                   name="shopping-bag"
                   size={30}
                   color="#3FC060"
                   style={{ marginHorizontal: 10 }}
                />
-               <Text style={styles.routeDetailsText}>5 km</Text>
+               <Text style={styles.routeDetailsText}>{totalKm.toFixed(1)} km</Text>
             </View>
 
             <View style={styles.deliveryDetailsContainer}>
@@ -64,9 +203,9 @@ const OrderDeliver = () => {
                </View>
             </View>
 
-            <View style={styles.buttonContainer}>
+            <Pressable onPress={onButtonPressed}  style={{...styles.buttonContainer, backgroundColor: isButtonDisabled() ? 'grey' : '#3FC060'}} disabled={isButtonDisabled}>
                <Text style={styles.buttonText}>Accept Order</Text>
-            </View>
+            </Pressable>
          </BottomSheet >
       </View >
    )
--- src/screens/OrderScreen/index.js
+++ src/screens/OrderScreen/index.js
@@ -1,9 +1,10 @@
 import { useEffect, useMemo, useRef, useState } from 'react'
-import { FlatList, View, Text, Dimensions, useWindowDimensions } from 'react-native'
+import { FlatList, View, Text, Dimensions, useWindowDimensions} from 'react-native'
 import MapView, { Marker } from 'react-native-maps';
 import BottomSheet from '@gorhom/bottom-sheet';
 import orders from '../../../assets/data/orders.json';
 import { Entypo } from "@expo/vector-icons"
+
 
 // components
 import OrderItem from '../../components/OrderItem';
@@ -11,30 +12,11 @@ import OrderItem from '../../components/OrderItem';
 
 const OrderScreen = () => {
 
-   const [driverLocation, setDriverLocation] = useState(null)
    const { width, height } = useWindowDimensions()
    const bottomSheetRef = useRef(null);
-   const snapPoints = useMemo(() => ["20%", "95%"], [])
+   const snapPoints = useMemo(() => ["25%", "95%"], [])
 
-   useEffect(() => {
-      (async () => {
-         
-         let { status } = await Location.getForegroundPermissionsAsync()
-         if (status == "granted") {
-            console.log('NONONO');
-            return;
-         }
-
-         let location = await Location.getCurrentPositionAsync()
-
-         setDriverLocation({
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude
-         });
-      })()
-   }, [])
-
-   console.log(driverLocation);
+  
 
    return (
       <View style={{ backgroundColor: 'lightblue', flex: 1 }}>
@@ -45,10 +27,7 @@ const OrderScreen = () => {
             }}
             showsUserLocation
             followsUserLocation
-            initialRegion={{
-               latitude: driverLocation?.latitude,
-               longitude: driverLocation?.longitude
-            }}
+           
          >
             {orders.map((order) => {
                return (
@@ -57,8 +36,8 @@ const OrderScreen = () => {
                      title={order.Restaurant.name}
                      description={order?.Restaurant?.address}
                      coordinate={{
-                        latitude: order.Restaurant.lat,
-                        longitude: order.Restaurant.lng
+                        latitude: order?.Restaurant?.lat,
+                        longitude: order?.Restaurant?.lng
                      }}
                   >
                      <View style={{ backgroundColor: 'green', borderRadius: 50, padding: 5 }}>
